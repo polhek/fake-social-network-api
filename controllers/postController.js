2d403b5852ddd0b197eaeb41cc5d7d2f1476af01
polhek <ziga.groselj@gmail.com>
2021-06-21 17:05:32 +0200

    req.files.file

--- controllers/postController.js
+++ controllers/postController.js
@@ -11,7 +11,7 @@ exports.newPost = async (req, res) => {
   const userId = req.user._id;
 
   try {
-    if (req.files.length > 0) {
+    if (req.files.file) {
       console.log('saving file to aws!!!!');
       const s3 = new aws.S3();
       const fileContent = Buffer.from(req.files.file.data, 'binary');
