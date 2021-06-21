725b7ebaf7abfed3458122a67cbcfc22490f1c9c
polhek <ziga.groselj@gmail.com>
2021-06-21 17:41:34 +0200

    fix

--- controllers/postController.js
+++ controllers/postController.js
@@ -11,7 +11,7 @@ exports.newPost = async (req, res) => {
   const userId = req.user._id;
 
   try {
-    if (!req.files) {
+    if (req.files.length === 0) {
       console.log('saving without file');
       const newPost = new Post({ user: userId, text: text });
       const post = await newPost.save();
