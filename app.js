6fcdf95592987f6ae457e6ba5c246c9bd1eb839d
polhek <ziga.groselj@gmail.com>
2021-07-01 16:55:03 +0200

    ss

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const fileUpload = require('express-fileupload');
+const watchMongoDb = require('./controllers/notificationController')
 require('dotenv').config();
 
 // Routes ...
@@ -34,6 +35,7 @@ const connectDB = async () => {
     });
 
     console.log(`Mongoose is running on: ${mongoDB}`);
+    watchMongoDb.watchPostChange()
   } catch (err) {
     console.log(`Failed to connect to ${mongoDB}!`);
   }
--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,10 +1,11 @@
-const socket = require('../utils/websockets');
+
 const Post = require('../models/post');
 
-
-
-const changePostStream = Post.watch();
+exports.watchPostChange = () =>{
+    const changePostStream = Post.watch();
 
 changePostStream.on('change', (change)=>{
     console.log(JSON.stringify(change))
-})
+});
+}
+
